ad081b9b33eba57bb882bd671686d794f6589a3f
Sqvdd <oscar.alshammar@gmail.com>
2024-03-10 00:52:57 +0100

    🚀 First Commit

---
+++ Installer.py
@@ -0,0 +1,37 @@
+from zipfile import ZipFile
+import requests
+import os
+
+
+user = ""   # Github Username
+repo = ""   # Repository Name
+filename = ""   # Zipfile to download
+
+
+url = f"https://github.com/{user}/{repo}/releases/latest/download/{filename}"   # Latest download link
+
+def downloadlink(url):
+    filename = url.split("/")[-1]
+    with open(filename, "wb") as file:
+        respons = requests.get(url)
+        file.write(respons.content)
+
+
+print("🚀 Downloading..")
+
+if url.endswith(".zip"):
+    downloadlink(url)
+else:
+    url = url + ".zip"
+    downloadlink(url)
+    filename += ".zip"
+
+
+print("| Done.")
+
+with ZipFile(filename, 'r') as zObject:
+    print("| Extracting .zip file...")
+    zObject.extractall(path="data")
+
+os.remove(filename)
+print("🚀 Installation completed.")
